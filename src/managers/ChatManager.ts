88545d314e6517d16d3f470dfb65c83225668915
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2025-03-11 14:42:45 +0000

    cleanup

--- src/managers/ChatManager.ts
+++ src/managers/ChatManager.ts
@@ -7,7 +7,7 @@ import type { ChatMessagePayload } from '../../resources/structs';
 function customUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8); // Asegura la variante correcta
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
